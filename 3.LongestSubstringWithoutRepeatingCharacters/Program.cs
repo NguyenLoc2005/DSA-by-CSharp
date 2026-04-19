21be265fa71977f1f39f10bbc5b6a10c8525f39b
NguyenLoc2005 <nxl522005@gmail.com>
2026-04-19 20:53:11 +0700

    LongestSubstringWithoutRepeatingCharacters

---
+++ 3.LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+class LongestSubstringWithoutRepeatingCharacters
+{
+    static void Main()
+    {
+        string s = "abcabcbb";
+
+        int count = 0;
+
+        for (int left = 0; left < s.Length; left++)
+        {
+            int maxLength = 0;
+            HashSet<char> chars = new HashSet<char>();
+
+            for (int right = left + 1; right < s.Length; right++)
+            {
+
+                if (chars.Contains(s[right]))
+                {
+                    break;
+                }
+                
+                chars.Add(s[right]);
+                maxLength++;
+                
+            }
+
+            if(maxLength > count)
+            {
+                count = maxLength;
+            }
+            
+       }
+
+        Console.WriteLine("" + count);
+    }
+}
