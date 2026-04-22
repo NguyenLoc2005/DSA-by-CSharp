36360989c45c7a3d6f7e1f37e417fb8b804ee9e0
NguyenLoc2005 <nxl522005@gmail.com>
2026-04-22 15:07:50 +0700

    Two Sum

--- 1.TwoSum/Program.cs
+++ 1.TwoSum/Program.cs
@@ -1,21 +1,21 @@
 ﻿
 
-class TwoSum
+class Solution
 {
-    static void Main(string[] args)
+    public int[] TwoSum(int[] nums, int target)
     {
-        int[] nums = { 1, 2, 3 };
-        int target = 4;
-
+        
         for (int i = 0; i < nums.Length; i++)
         {
             for (int j = i + 1; j < nums.Length; j++)
             {
                 if (nums[i] + nums[j] == target)
                 {
-                    Console.WriteLine("Index:" + i + "," + j);
+                    
+                    return new int[] { i, j };
                 }
             }
         }
+        return new int[] { -1, -1 };
     }
 }
